e5a148231568c423de98c3059495ed32696d9a4e
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-25 17:27:49 -0500

    instanciado para servir de archivos html estaticos de la carpeta public

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = 3000;
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
