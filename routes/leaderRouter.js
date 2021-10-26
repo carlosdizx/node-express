4268e15f5d15fa64dfe29b8bae5519e5fc16ecab
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-26 11:14:56 -0500

    Construido el archivo con la logica de rutas requeridad para los crud de la entidad leader

---
+++ routes/leaderRouter.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter
+  .route("/")
+
+  .get((request, response, next) => {
+    response.end("Will send all the leaders to you!");
+  })
+
+  .post((request, response, next) => {
+    response.end(
+      "Will add the leaders: " +
+        request.body.name +
+        " with details: " +
+        request.body.description
+    );
+  })
+
+  .put((request, response, next) => {
+    response.statusCode = 403;
+    response.end("PUT operation not supported on /leaders");
+  })
+
+  .delete((request, response, next) => {
+    response.end("Deleting all the leaders!");
+  });
+
+leaderRouter
+  .route("/:leaderId")
+  .get((request, response, next) => {
+    response.end(
+      "Will send details of the leader: " + request.params.leaderId + " to you!"
+    );
+  })
+
+  .post((request, response, next) => {
+    response.statusCode = 403;
+    response.end(
+      "POST operation not supported on /leaders/" + request.params.leaderId
+    );
+  })
+
+  .put((request, response, next) => {
+    response.write("Updating the leader: " + request.params.leaderId + "\n");
+    response.end(
+      "Will update the leader: " +
+        request.body.name +
+        " with details: " +
+        request.body.description
+    );
+  })
+
+  .delete((request, response, next) => {
+    response.end("Deleting leader:" + request.params.leaderId);
+  });
+
+module.exports = leaderRouter;
